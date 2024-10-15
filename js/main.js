48fbbd1e906bac5f563defc810c7a8afb777b04b
GalloSimone <gallosimonecz@gmail.com>
2024-10-15 23:26:58 +0200

    end work

--- js/main.js
+++ js/main.js
@@ -7,4 +7,16 @@ for(i=0;i<100;i++){
     const div3 = num % 3 === 0;
     const div5 = num % 5 === 0;
   
+    if (div3 ) {
+        output = "Fizz";
+      }
+
+      else if(div5 ){
+        output = "buzz"
+      }
+      else if (div3 && div5) {
+        output = "FizzBuzz";
+      }
+      console.log(output);
+      
 }
